733d3a0bb547866e7707ace3534884919bf33f5d
anneke.huijsmans <anneke.huijsmans@FWIN-07.internal.fourtress.nl>
2018-07-12 15:09:08 +0200

    completed consuming rest guide

---
+++ src/main/java/RelationalDataAccessGuide/Customer.java
@@ -0,0 +1,5 @@
+package RelationalDataAccessGuide;
+
+public class Customer {
+
+}
--- src/main/java/ScheduledTaskGuide/ScheduledTasks.java
+++ src/main/java/ScheduledTaskGuide/ScheduledTasks.java
@@ -1,4 +1,4 @@
-package hello;
+package ScheduledTaskGuide;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
